357b96239edb14b65af659fc93ac8eeba9bf5cca
Phil Huynh <philiphuynh98@gmail.com>
2022-09-22 15:17:00 -0400

    update install

--- install/run_install.sh
+++ install/run_install.sh
@@ -1,27 +1,28 @@
 #! /bin/bash
 
   if [ -n "$ZSH_VERSION" ]; then
-    if [ -f ~/.zshrc ]; then
-      cat ~/quickstarter/install/rc_update.sh >> ~/.zshrc
-    elif [ -f ~/.zshenv]; then
-      cat ~/quickstarter/install/profile_update.sh >> ~/.zshenv
-      touch ~/.zshrc
-      cat ~/quickstarter/install/rc_update.sh >> ~/.zshrc
-    else
-      touch ~/.zshrc
-      cat ~/quickstarter/install/rc_update.sh >> ~/.zshrc
-    fi
+      if [ -f ~/.zshrc ]; then
+        cat ~/quickstarter/install/rc_update.sh >> ~/.zshrc
+      elif [ -f ~/.zshenv]; then
+        cat ~/quickstarter/install/profile_update.sh >> ~/.zshenv
+        touch ~/.zshrc
+        cat ~/quickstarter/install/rc_update.sh >> ~/.zshrc
+      else
+        touch ~/.zshrc
+        cat ~/quickstarter/install/rc_update.sh >> ~/.zshrc
+      fi
   elif [ -n "$BASH_VERSION" ]; then
-    if [ -f ~/.bashrc ]; then
-      cat ~/quickstarter/install/rc_update.sh >> ~/.bashrc
-    elif [ -f ~/.bash_profile ]; then
-      cat ~/quickstarter/install/profile_update.sh >> ~/.bash_profile
-      touch ~/.bashrc
-      cat ~/quickstarter/install/rc_update.sh >> ~/.bashrc
-    else
-      touch ~/.bashrc
-      cat ~/quickstarter/install/rc_update.sh >> ~/.bashrc
-    fi
+      if [ -f ~/.bashrc ]; then
+        cat ~/quickstarter/install/rc_update.sh >> ~/.bashrc
+      elif [ -f ~/.bash_profile ]; then
+        cat ~/quickstarter/install/profile_update.sh >> ~/.bash_profile
+        touch ~/.bashrc
+       cat ~/quickstarter/install/rc_update.sh >> ~/.bashrc
+      else
+        touch ~/.bashrc
+        cat ~/quickstarter/install/rc_update.sh >> ~/.bashrc
+      fi
   else
+    echo "Not bash or zsh"
   fi
 
